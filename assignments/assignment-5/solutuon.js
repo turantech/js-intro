4516ad94b18e0454ea7aa90f425f8964b80d93e0
Ali Ramazon <mr.safarali@gmail.com>
2023-06-03 18:58:05 -0600

    Provide solution for assignment-5

---
+++ assignments/assignment-5/solutuon.js
@@ -0,0 +1,16 @@
+// Variables declaration
+var name = "John";
+var age = 30;
+var city = "New York";
+var profession = "software engineer";
+var hobby1 = "reading books";
+var hobby2 = "playing the guitar";
+var favoriteFood = "pizza";
+var favoriteColor = "blue";
+var dreamDestination = "Tokyo";
+var favoriteSeason = "autumn";
+
+// Paragraph assembly using template literals
+console.log(
+    `Hello, my name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time, I enjoy ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${favoriteColor}. My dream is to one day visit ${dreamDestination}. My favorite season of the year is ${favoriteSeason} because of the beautiful changing colors of the leaves.`
+);
