7ad5ec66471ad94338230df5de1bbc37d9162ec1
Ali Ramazon <mr.safarali@gmail.com>
2023-06-06 08:31:03 -0600

    Finish assignment-8

---
+++ assignments/assignment-8/solution.js
@@ -0,0 +1,29 @@
+// Between 1 and 10
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// Between 1 and 100
+console.log(Math.floor(Math.random() * 100) + 1);
+
+// Between 50 and 100
+console.log(Math.floor(Math.random() * 51) + 50);
+
+// Between -10 and 10
+console.log(Math.floor(Math.random() * 21) - 10);
+
+// Between -100 and 100
+console.log(Math.floor(Math.random() * 201) - 100);
+
+// Between 1000 and 2000
+console.log(Math.floor(Math.random() * 1001) + 1000);
+
+// Between -1000 and 0
+console.log(Math.floor(Math.random() * 1001) - 1000);
+
+// Between 1 and 1000
+console.log(Math.floor(Math.random() * 1000) + 1);
+
+// Between -500 and 500
+console.log(Math.floor(Math.random() * 1001) - 500);
+
+// Between 10000 and 20000
+console.log(Math.floor(Math.random() * 10001) + 10000);
