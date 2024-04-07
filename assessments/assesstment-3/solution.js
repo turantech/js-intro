4aa5dd38fb0bd2a220d5dc3a8a9f624e0441610a
Ali Ramazon <mr.safarali@gmail.com>
2024-04-07 15:30:48 -0600

    Solve assessment-3, challenge-5 missing

---
+++ assessments/assesstment-3/solution.js
@@ -0,0 +1,72 @@
+/* ---- 1 ---- */
+var iLoveJS = "I Love JavaScript";
+var someString = "I Love JavaScript and HTML,CSS";
+
+iLoveJS = iLoveJS.replaceAll(" ", "");
+someString = someString.replaceAll(" ", "");
+// console.log(iLoveJS);
+// console.log(someString);
+
+/* ---- 2 ---- */
+var javascript = "javascript";
+// "j".toUpperCase() + "ava" + "s".toUpperCase() + "cript" => "JavaScript"
+
+var firstSIdx = javascript.indexOf("s");
+
+var part1 = javascript[0].toUpperCase();
+var part2 = javascript.slice(1, firstSIdx);
+var part3 = javascript[firstSIdx].toUpperCase();
+var part4 = javascript.slice(firstSIdx + 1);
+
+javascript = part1 + part2 + part3 + part4;
+
+// console.log(javascript);
+
+/* ---- 3 ---- */
+var fullName1 = "Will Smith";
+//  "WS"
+
+var fullName2 = "Barack Obama";
+// "BO"
+
+var fullName3 = "Taylor Swift";
+// "TS"
+
+// Bad console.log(fullName1[0] + fullName1[5]);
+
+var firstNameInitial = fullName1[0];
+var spaceIdx = fullName1.indexOf(" ");
+var lastNameInitial = fullName1[spaceIdx + 1];
+var initial = firstNameInitial + lastNameInitial;
+// console.log(initial);
+
+/* ---- 4 ---- */
+// 1
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// var firstLetterOfMyName = "a";
+var nameFirstLetter = "z";
+var nameFirstLetterIdx = alphabet.indexOf(nameFirstLetter);
+
+var alphabetWithoutMyNameFirstCharacter1 =
+    alphabet.slice(0, nameFirstLetterIdx) +
+    alphabet.slice(nameFirstLetterIdx + 1);
+
+// console.log(alphabetWithoutMyNameFirstCharacter1);
+
+// 2
+var alphabetWithoutMyNameFirstCharacter2 = alphabet.replace(
+    nameFirstLetter,
+    ""
+);
+// console.log(alphabetWithoutMyNameFirstCharacter2);
+
+/* ---- 6 ---- */
+
+var randomString = "I live in NY. NY is rainyaa";
+var randomChar = "a";
+var randomStringLength = randomString.length;
+var randomStringWithoutRandomChar = randomString.replaceAll(randomChar, "");
+
+var randomCharCount = randomStringLength - randomStringWithoutRandomChar.length;
+
+console.log(randomCharCount);
